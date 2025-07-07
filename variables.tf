bb489019f9a72f005daca953d16b67fc5721ea75
sadiaijaz123 <sadia.ijaz@microtechx.com>
2025-07-08 02:34:34 +0500

    Create variables.tf
    Add Terraform files for Conditional Access deployment

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "client_id" {
+  type        = string
+  description = "Azure App client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure App client secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
